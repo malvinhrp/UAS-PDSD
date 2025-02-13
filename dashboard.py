c52108fe32e2e7dbb0f8f464b4fdcf587c53d358
malvinhrp <semutbilu@gmail.com>
2025-02-14 01:32:20 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+st.set_page_config(layout="wide")
+
 
 # Membaca data dari file CSV
 data1 = pd.read_csv('hour.csv')
