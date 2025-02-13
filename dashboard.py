39de23d8e1f2d3d25f569bdc8913933c1290cc2e
malvinhrp <semutbilu@gmail.com>
2025-02-14 01:49:25 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -49,7 +49,7 @@ if st.button("Tampilkan"):
         ax.set_title('Rata-rata Penyewaan per Jam')
         ax.set_xlabel('Jam')
         ax.set_ylabel('Rata-rata Penyewaan')
-        ax.figure(figsize=(5, 3))
+        plt.figure(figsize=(5, 3))
         st.pyplot(fig)
 
 
