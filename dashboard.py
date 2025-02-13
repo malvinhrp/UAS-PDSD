181a89d3550f6daedf9e385d7146e83d43bae21d
malvinhrp <semutbilu@gmail.com>
2025-02-14 01:59:02 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -44,7 +44,7 @@ if st.button("Tampilkan"):
         st.subheader("Rata-rata Penyewaan per Jam")
         st.write("""Pada visualisasi ini, kita dapat melihat grafik yang menampilkan rata-rata penyewaan sepeda per jam """)
         rata_rata_penyewaan_per_jam = data1.groupby('hr')['cnt'].mean()
-        fig, ax = plt.subplots(figsize=(2, 1))
+        fig, ax = plt.subplots()
         rata_rata_penyewaan_per_jam.plot(kind='bar', color='red', edgecolor='black', ax=ax)
         ax.set_title('Rata-rata Penyewaan per Jam')
         ax.set_xlabel('Jam')
