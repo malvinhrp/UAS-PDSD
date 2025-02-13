be06b9261017f5b17a277d0bdd0d6dd1101933b2
malvinhrp <semutbilu@gmail.com>
2025-02-14 02:07:58 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-st.set_page_config(layout="1:1")
+st.set_page_config(layout="wide")
 
 
 # Membaca data dari file CSV
